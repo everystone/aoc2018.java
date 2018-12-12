90d0c9cbec091320f538c03b449f538a19ed95e8
everystone <eirik.kvarstein@marcello.no>
2018-12-12 10:40:07 +0100

    Day 2 Part 2

--- src/main/java/solutions/DayTwo.java
+++ src/main/java/solutions/DayTwo.java
@@ -1,28 +1,16 @@
 package solutions;
 
-
 import common.FileUtils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 // revtoubfniyhzsgxdowjwkqglp
-class BoxID {
-  public boolean hasTwo;
-  public boolean hasThree;
-  private String[] letters;
-  BoxID(String str) {
-    this.letters = str.split("");
-    this.hasTwo = Arrays.stream(letters)
-      .anyMatch(c -> count(c) == 2);
 
-    this.hasThree = Arrays.stream(letters)
-      .anyMatch(c -> count(c) == 3);
-  }
-  private long count(String c) {
-    return Arrays.stream(this.letters).filter(l -> l.equals(c)).count();
-  }
-}
 
 public class DayTwo {
   private long charCount(String c, String[] src) {
@@ -31,17 +19,44 @@ public class DayTwo {
   public static void main(String[] args) {
     String[] lines = FileUtils.ReadLines("./puzzles/day2.txt");
 
-    long twoCount = Arrays.stream(lines)
-      .map(BoxID::new)
-      .filter(l -> l.hasTwo)
+    // count number of line that have exactly 2 character duplicates.
+    long twoCount = Arrays.stream(lines).filter(line -> line.chars()
+      .anyMatch(c1 -> line.chars()
+        .filter(c2 -> c1 == c2)
+        .count() == 2))
       .count();
 
-    long threeCount = Arrays.stream(lines)
-      .map(BoxID::new)
-      .filter(i -> i.hasThree)
+    long threeCount = Arrays.stream(lines).filter(line -> line.chars()
+      .anyMatch(c1 -> line.chars()
+        .filter(c2 -> c1 == c2)
+        .count() == 3))
       .count();
 
     long part1 = twoCount * threeCount;
     System.out.println("Part 1: " + part1);
+
+    // compare every line with each other, and find the two that differs by 1 character.
+    List<String> results = Arrays.stream(lines)
+      .filter(line1 -> Arrays.stream(lines)
+      .anyMatch(line2 -> getDiff(line1, line2) == 1))
+      .collect(Collectors.toList());
+
+
+    String part2 = "";
+    String a = results.get(0);
+    String b = results.get(1);
+    for(int i=0;i<a.length();i++){
+      if (b.charAt(i) == a.charAt(i)) {
+        part2 += a.charAt(i);
+      }
+    }
+    System.out.println("Part 2: " + part2);
+}
+
+  // returns number of differences between a and b
+  static long getDiff(String a, String b) {
+    return IntStream.range(0, a.length())
+      .filter(i -> a.charAt(i) != b.charAt(i))
+      .count();
   }
 }
